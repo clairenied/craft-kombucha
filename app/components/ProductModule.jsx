b8ea9121f33c99e340399756c6b2ec7ae0e6d6ef
clairernied <clairenied@gmail.com>
2017-01-10 17:21:45 -0600

    fleshed out rest of skeleton except for admin page

--- app/components/ProductModule.jsx
+++ app/components/ProductModule.jsx
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router'
+
 import SingleProduct from './SingleProduct';
 
 const ProductModule = (props) => {
   return (
     <div className="col-xs-6 col-s-3 col-md-2 col-lg-2 col-xl-2">
-      <img src="http://brewdrkombucha.com/2016/wp-content/uploads/2016/04/organic-raw-brew-dr-kombucha-clear-mind.png" className="img-responsive"/>
+      <Link to="/single-product">
+        <img src="http://brewdrkombucha.com/2016/wp-content/uploads/2016/04/organic-raw-brew-dr-kombucha-clear-mind.png" className="img-responsive"/>
+      </Link>
       <h4>Card title</h4>
       <p>Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <a href="#" className="btn btn-primary">Add To Cart</a>
     </div>
   )
 }
--- app/components/ReviewModule.jsx
+++ app/components/ReviewModule.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const AllReviews = (props) => {
+const ReviewModule = (props) => {
 	return (
 		<div>
+			<h4><b>Title</b></h4>
 			<p><b>First name</b>
-			<b>******</b></p>
-
-			<p><b>Title</b><p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis volutpat dolor, vitae mollis dolor congue vel. Suspendisse tortor erat, vestibulum a lorem id, dictum tempor enim. Donec aliquam rutrum nisl nec aliquam. Cras accumsan purus dolor, eget ullamcorper augue fermentum eu. Duis quis mi sagittis ipsum aliquam hendrerit in sit amet velit.
-			</p> 
-
-			<p>Read More</p>
 			<p>Date created</p>
+			<b>******</b></p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis volutpat dolor, vitae mollis dolor congue vel. Suspendisse tortor erat, vestibulum a lorem id, dictum tempor enim. Donec aliquam rutrum nisl nec aliquam. Cras accumsan purus dolor, eget ullamcorper augue fermentum eu. Duis quis mi sagittis ipsum aliquam hendrerit in sit amet velit.
+			</p>
+      <Link to="/single-review">More...</Link>
 		</div>
 	)
 }
 
-export default AllReviews
+export default ReviewModule
