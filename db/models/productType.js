59a9bf2131317dc2d20469b8cc2d6f866294bbe5
KdawgG <kamaldeep.gandhi@gmail.com>
2017-01-10 13:59:02 -0600

    adding hooks..

--- db/models/productType.js
+++ db/models/productType.js
@@ -11,10 +11,18 @@ const ProductType = db.define('producttypes', {
     allowNull: false,
   },
 }, {
-  // TO DO: COME BACK AND UPDATE MEEEEEEEEEEE
-  // hooks: {
-  //   beforeCreate: setRating
-  // }
+   instanceMethods: {
+    getRating: function(){
+      this.getReviews()
+      .then(reviews=>{
+        let sum = 0;
+        reviews.forEach(review=>{
+          sum+=review.starRating
+        })
+        return sum/reviews.length
+      })
+    }
+   }
 
   // We support oauth, so users may or may not have passwords.
 });
