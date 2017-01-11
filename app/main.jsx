2a781203535cb4e7e157669f77cd083aa16a0655
Jean Chung <jxchung@gmail.com>
2017-01-11 16:11:49 -0600

    loading users data in admin components

--- app/main.jsx
+++ app/main.jsx
@@ -1,27 +1,34 @@
-'use strict'
-import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
-import {render} from 'react-dom'
-import {connect, Provider} from 'react-redux'
+import React from 'react';
+import { Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import { connect, Provider } from 'react-redux';
+import axios from 'axios';
 
-import store from './store'
+import store from './store';
 
-//Components
-// import Jokes from './components/Jokes'
-import Navbar from './components/Navbar'
-import Products from './components/Products'
-import Login from './components/Login'
-import WhoAmI from './components/WhoAmI'
-import SingleProduct from './components/SingleProduct'
-import SingleReview from './components/SingleReview'
-import Signup from './components/Signup'
-import Order from './components/Order'
-import Cart from './components/Cart'
-import Admin from './components/Admin'
+// Components
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import Login from './components/Login';
+import WhoAmI from './components/WhoAmI';
+import SingleProduct from './components/SingleProduct';
+import SingleReview from './components/SingleReview';
+import Signup from './components/Signup';
+import Order from './components/Order';
+import Cart from './components/Cart';
+import Admin from './components/Admin';
+
+// Actions
+import { fetchUsers } from './reducers/users';
+
+// On-Enter Hooks
+const adminOnEnter = (nextRouterState) => {
+  store.dispatch(fetchUsers());
+};
 
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
-) (
+)(
   ({ user, children }) =>
     <div>
       <Navbar />
@@ -29,7 +36,7 @@ const ExampleApp = connect(
         {children}
       </div>
     </div>
-)
+);
 
 render (
   <Provider store={store}>
@@ -42,9 +49,9 @@ render (
         <Route path="/single-review" component={SingleReview} />
         <Route path="/signup" component={Signup} />
         <Route path="/cart" component={Cart} />
-        <Route path="/admin" component={Admin} />
+        <Route path="/admin" component={Admin} onEnter={adminOnEnter} />
       </Route>
     </Router>
   </Provider>,
   document.getElementById('main')
-)
+);
