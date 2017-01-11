fad9d0ce0cd9689047cd7c533a0cb6a1d8534a17
Jean Chung <jxchung@gmail.com>
2017-01-11 13:21:20 -0600

    added users reducer and action-creators

--- app/components/Admin.jsx
+++ app/components/Admin.jsx
@@ -1,19 +1,22 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import AdminModule from './AdminModule';
 
-const Admin = (props) => {
-  return (
-    <div>
-      <div className="page-header col-xs-12">
-        <h1>Admin</h1>
-      </div>
-      <AdminModule/>
-      <AdminModule/>
-      <AdminModule/>
-      <AdminModule/>
+const Admin = props => (
+  <div>
+    <div className="page-header col-xs-12">
+      <h1>Admin</h1>
     </div>
-  )
-}
+    <AdminModule/>
+    <AdminModule/>
+    <AdminModule/>
+    <AdminModule/>
+  </div>
+);
 
-export default Admin;
+const mapStateToProps = state => {
+  const { allUsers } = state.users;
+  return { allUsers };
+};
+
+export default connect(mapStateToProps)(Admin);
