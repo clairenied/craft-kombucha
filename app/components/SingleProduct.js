e84f31282f25de94b51c157bd7684dbcd7353c1f
clairernied <clairenied@gmail.com>
2017-01-11 17:21:14 -0600

    getting single product to render

--- app/components/SingleProduct.js
+++ app/components/SingleProduct.js
@@ -6,25 +6,27 @@ import ReviewModule from './ReviewModule'
 
 const SingleProduct = (props) => {
   const singleProduct = props.products.singleProduct
-  const productType = props
   console.log('PROOOOOOPS', props)
   return(
 		<div>
 			<div className="page-header col-xs-12">
-        <h1>Product Name</h1>
+        <h1>{singleProduct.producttype.name}</h1>
       </div>
       <div className="col-xs-12 col-sm-4">
         <img src="http://brewdrkombucha.com/2016/wp-content/uploads/2016/04/organic-raw-brew-dr-kombucha-clear-mind.png" className="img-responsive"/>
       </div>
       <div className="col-xs-12 col-sm-8">
         <h4 className="text-uppercase">Product Description</h4>
-        <h4>{singleProduct.price}</h4>
         <p>
-          In ullamcorper rutrum quam ut interdum. Vivamus ac condimentum ex. Nam tristique purus vitae arcu ornare, sit amet auctor turpis vehicula. Ut posuere luctus vestibulum. In consectetur aliquam sem at rutrum. Nam sit amet mattis mi. Quisque enim quam, rutrum tempor nisl eu, molestie porttitor arcu. Mauris dignissim, ipsum ac bibendum molestie, augue ipsum finibus dolor, non pharetra turpis sem ac ligula. Sed aliquet sollicitudin quam, eu blandit libero fringilla vitae. Curabitur pharetra urna vel orci lobortis, id bibendum metus congue. Aliquam hendrerit velit vitae elit finibus pharetra. Phasellus sem risus, aliquet non elit at, porta vulputate sem. Duis tempor purus id ipsum tempor rutrum.
+          {singleProduct.size}
+        </p>
+        <p>
+          {singleProduct.producttype.description}
         </p>
         <p>
           Act fast! Only <b>{singleProduct.remaining}</b> remaining
         </p>
+        <a href="#" className="btn btn-default">${singleProduct.price}</a>
       </div>
       <div className="row col-xs-12">
         <div className="page-header col-xs-12">
