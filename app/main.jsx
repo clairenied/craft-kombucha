3cbf2945a3c9f87a9aec64818357ad3a12819a03
clairernied <clairenied@gmail.com>
2017-01-12 09:51:05 -0600

    making some changes to routes

--- app/main.jsx
+++ app/main.jsx
@@ -62,20 +62,18 @@ render (
     <Router history={browserHistory}>
       <Route path="/" component={ExampleApp}>
         <IndexRedirect to="/products" />
-        <Route path="/products" component={ProductsContainer} />
-        <Route path="/single-review" component={SingleReview} />
         <Route path="/signup" component={Signup} />
-
-        // <Route path="/orders" component={Order} 
-               onEnter={onOrdersEnter}/>         
-        <Route path="/orders/:orderId" component={Order} />
         
-        <Route path="/admin" component={Admin} />
         <Route path="/products" component={ProductsContainer} onEnter={loadAllProducts} />
         <Route path="/products/:productId" component={SingleProductContainer} onEnter={loadSingleProduct} />
+        
         <Route path="/single-review" component={SingleReview} />
-        <Route path="/signup" component={Signup} />
+
+        <Route path="/orders" component={Order} 
+               onEnter={onOrdersEnter}/>         
+        <Route path="/orders/:orderId" component={Order} />
         <Route path="/cart" component={Cart} />
+        
         <Route path="/admin" component={Admin} onEnter={adminOnEnter} />
       </Route>
     </Router>
