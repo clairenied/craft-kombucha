34f707e736ec53f73e0262acc95dca53d4803dad
Alexis J <alexis.m.jenn@gmail.com>
2017-01-17 16:10:04 -0600

    minor changes

--- app/components/SingleOrder.js
+++ app/components/SingleOrder.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import { Link } from 'react-router'
-
 import SingleOrderModule from './SingleOrderModule'
 
 const SingleOrder = (props) => {
