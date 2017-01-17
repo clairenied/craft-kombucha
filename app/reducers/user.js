78d403da4057e710c6f4f9a02806bbbe66a06295
Jean Chung <jxchung@gmail.com>
2017-01-17 10:14:43 -0600

    signup page integrated with auth

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 
+import { login } from '../reducers/auth';
+
 const initialState = {
-  loggedIn: false,
-  amAdmin: false,
-  name: '',
   error: '',
 };
 
@@ -13,9 +12,11 @@ const SIGNUP_FAIL = 'SIGNUP_FAIL';
 
 export const signUpForAccount = (userAndAddress) => {
   return (dispatch) => {
+    const { email, password } = userAndAddress.userInfo;
     axios.post('/api/users', userAndAddress)
-      .then((res) => {
-        dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+      .then(() => {
+        dispatch({ type: SIGNUP_SUCCESS });
+        dispatch(login(email, password));
         browserHistory.push('/products');
       })
       .catch(err => dispatch({ type: SIGNUP_FAIL, payload: err }));
@@ -27,8 +28,6 @@ export default (state = initialState, action) => {
   const nextState = Object.assign({}, state);
   switch (action.type) {
     case SIGNUP_SUCCESS:
-      nextState.loggedIn = true;
-      nextState.name = action.payload.firstName;
       nextState.error = '';
       break;
     case SIGNUP_FAIL:
